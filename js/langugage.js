e921174f31b5d56ca913c9661b29daf37650f8fb
ismaelDeveloper <ismael.hadj13@gmail.com>
2024-09-02 17:45:10 +0900

    translation second recommandation letter

--- js/langugage.js
+++ js/langugage.js
@@ -25,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
       toggleSwitch.addEventListener('change', function() {
         this.checked ? document.getElementById("resume_dl").href = "./Ismael-HADJ-resume-FR.pdf" : document.getElementById("resume_dl").href = "./Ismael-HADJ-resume-EN.pdf"
         this.checked ? document.getElementById("recommandation_letter_1").href = "./recommandation_letter_fr.pdf" : document.getElementById("recommandation_letter_1").href = "./추천서_이스마엘.pdf"
+        this.checked ? document.getElementById("recommandation_letter_EPS").href = "./Ismael_Hadj_EPS_recommandation_letter.pdf" : document.getElementById("recommandation_letter_EPS").href = "./추전서_EPS_엔지니어링.pdf"
         console.log(document.getElementById("recommandation_letter_1").href)
         changeLanguage(this.checked ? 'fr' : 'en');
       });
